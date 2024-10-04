eb46623b6529c6aa3b10803205bc4bfc72938c6c
Andres-XL <amovillaw01@gmail.com>
2024-10-03 20:16:06 -0500

    add rows and cols support

--- src/_components/json-formatter.tsx
+++ src/_components/json-formatter.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { useState, useEffect, useCallback, Fragment } from "react";
-import { AlertCircle, Check, Copy } from "lucide-react";
+import { AlertCircle, Check, Columns2, Copy, Rows2Icon } from "lucide-react";
 import locale from "~/utils/locale.json";
 import type LocaleStrings from "~/utils/types";
+import clsx from "clsx";
 
 const debounce = (func: (arg: string) => void, delay: number) => {
   let timeoutId: NodeJS.Timeout;
@@ -18,6 +19,7 @@ export default function JSONFormatter({ lang = "en" }: { lang?: "en" | "es" }) {
   const [output, setOutput] = useState("");
   const [error, setError] = useState("");
   const [isCopied, setIsCopied] = useState(false);
+  const [showColumns, setShowColumns] = useState(true);
   const localeStrings = locale[lang] as LocaleStrings;
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -60,11 +62,50 @@ export default function JSONFormatter({ lang = "en" }: { lang?: "en" | "es" }) {
 
   return (
     <div className="container mx-auto p-4 pt-16">
-      <h1 className="mb-4 text-center text-2xl font-bold">
-        {localeStrings.title}
-      </h1>
-      <div className="mx-auto grid max-w-3xl grid-cols-1 gap-4 md:grid-cols-2">
-        <div className="space-y-4">
+      <div className="relative mx-auto flex max-w-screen-md items-center justify-center">
+        <h1 className="mb-4 text-center text-2xl font-bold">
+          {localeStrings.title}
+        </h1>
+
+        <div className="absolute right-0 top-0 flex w-fit rounded-md border border-gray-200">
+          <button
+            className={clsx([
+              "px-2 py-1",
+              {
+                "bg-gray-200": showColumns,
+                "opacity-50": !showColumns,
+              },
+            ])}
+            onClick={() => setShowColumns(true)}
+          >
+            <Columns2 />
+          </button>
+
+          <button
+            className={clsx([
+              "px-2 py-1",
+              {
+                "bg-gray-200": !showColumns,
+                "opacity-50": showColumns,
+              },
+            ])}
+            onClick={() => setShowColumns(false)}
+          >
+            <Rows2Icon />
+          </button>
+        </div>
+      </div>
+
+      <div
+        className={clsx([
+          "mx-auto flex max-w-3xl gap-4",
+          {
+            "flex-row": showColumns,
+            "flex-col": !showColumns,
+          },
+        ])}
+      >
+        <div className="flex-1 space-y-4">
           <h2 className="text-lg font-semibold">
             {localeStrings.labels.input}
           </h2>
@@ -72,10 +113,16 @@ export default function JSONFormatter({ lang = "en" }: { lang?: "en" | "es" }) {
             placeholder={localeStrings.labels.data}
             value={input}
             onChange={(e) => setInput(e.target.value)}
-            className="h-[calc(100vh-200px)] min-h-[300px] w-full resize-none rounded-md border border-gray-200 p-4 outline-none transition focus:border-blue-400"
+            className={clsx([
+              "w-full resize-none rounded-md border border-gray-200 p-4 outline-none transition focus:border-blue-400",
+              {
+                "h-[calc(100vh-200px)] min-h-[300px]": showColumns,
+                "h-[calc(45vh-100px)] min-h-[300px]": !showColumns,
+              },
+            ])}
           />
         </div>
-        <div className="space-y-4">
+        <div className="flex-1 space-y-4">
           <div className="flex items-center justify-between">
             <h2 className="text-lg font-semibold">
               {localeStrings.labels.output}
@@ -83,10 +130,12 @@ export default function JSONFormatter({ lang = "en" }: { lang?: "en" | "es" }) {
             {output && (
               <button
                 onClick={handleCopy}
-                className={
-                  "flex items-center gap-2 rounded-md border border-gray-200 px-2 py-1 outline-none transition hover:bg-gray-200" +
-                  (isCopied ? "bg-green-600 text-white" : "")
-                }
+                className={clsx([
+                  "flex items-center gap-2 rounded-md border border-gray-200 px-2 py-1 outline-none transition hover:bg-gray-200",
+                  {
+                    "bg-green-600 text-white": isCopied,
+                  },
+                ])}
               >
                 {isCopied ? (
                   <Fragment>
@@ -103,7 +152,14 @@ export default function JSONFormatter({ lang = "en" }: { lang?: "en" | "es" }) {
             )}
           </div>
 
-          <div className="relative h-[calc(100vh-200px)] min-h-[300px]">
+          <div
+            className={clsx([
+              {
+                "relative h-[calc(100vh-200px)] min-h-[300px]": showColumns,
+                "relative h-[calc(45vh-100px)] min-h-[300px]": !showColumns,
+              },
+            ])}
+          >
             {error ? (
               <div className="absolute inset-0 flex flex-col items-center justify-center rounded-md border border-red-500 p-4 text-red-500">
                 <AlertCircle className="absolute left-5 top-5 h-4 w-4" />
